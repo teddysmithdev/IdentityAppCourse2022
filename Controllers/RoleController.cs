1220eab3042cecc514c55fe9aa9d4f8bcacad85e
pythonapptester <pythonapptester@gmail.com>
2022-06-25 07:36:17 -0400

    Role Crud

---
+++ Controllers/RoleController.cs
@@ -0,0 +1,97 @@
+﻿using IdentityAppCourse2022.Data;
+using IdentityAppCourse2022.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityAppCourse2022.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+
+        public RoleController(ApplicationDbContext db, UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+
+        public IActionResult Index()
+        {
+            var roles = _db.Roles.ToList();
+            return View(roles);
+        }
+
+        [HttpGet]
+        public IActionResult Upsert(string id)
+        {
+            if (String.IsNullOrEmpty(id))
+            {
+                return View();
+            }
+            else
+            {
+                //update
+                var user = _db.Roles.FirstOrDefault(u => u.Id == id);
+                return View(user);
+            }
+
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(IdentityRole role)
+        {
+            if (await _roleManager.RoleExistsAsync(role.Name))
+            {
+                //error
+                return RedirectToAction(nameof(Index));
+            }
+            if (string.IsNullOrEmpty(role.Id))
+            {
+                //create
+                await _roleManager.CreateAsync(new IdentityRole() { Name = role.Name });
+            }
+            else
+            {
+                //update
+                var roleDb = _db.Roles.FirstOrDefault(u => u.Id == role.Id);
+                if (roleDb == null)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                roleDb.Name = role.Name;
+                roleDb.NormalizedName = role.Name.ToUpper();
+                var result = await _roleManager.UpdateAsync(roleDb);
+            }
+            return RedirectToAction(nameof(Index));
+
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var roleDb = _db.Roles.FirstOrDefault(u => u.Id == id);
+            if (roleDb == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            var userRolesForThisRole = _db.UserRoles.Where(u => u.RoleId == id).Count();
+            if (userRolesForThisRole > 0)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            await _roleManager.DeleteAsync(roleDb);
+            return RedirectToAction(nameof(Index));
+
+        }
+    }
+}
